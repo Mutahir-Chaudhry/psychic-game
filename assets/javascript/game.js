0732b6f4b4731d7c33714964d1083c286e15f0f7
Mutahir-Chaudhry <mutachaudhry@gmail.com>
2019-06-18 20:32:18 -0400

    Psuedo Code

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+// 3. Guess what letter I'm thinking of
+// First Step: computer has to guess a letter. The letters has to be stored in a variable. 
+
+// 4. Wins: (# of times the user has guessed the letter correctly)
+//Want user to see wins and the number of time letters guessed correctly. Create variable for wins in HTML user can see
+
+// 5. Losses: (# of times the user has failed to guess the letter correctly after exhausting all guesses)
+//Want user to see loses and the number of time letters guessed correctly. Create variable for loses in HTML user can see
+
+// 6. Guesses Left: (# of guesses left. This will update)
+//Want the number of guesses left. Create variable for guesses in HTML user can see
+
+// 7. Your Guesses So Far: (the specific letters that the user typed. Display these until the user either wins or loses.)
+//Want to see the letters that the user has guessed so far. 
+
+// 8. When the player wins, increase the Wins counter and start the game over again (without refreshing the page).
+//Take win and add to existing wins being displayed in HTML
+
+// 9. When the player loses, increase the Losses counter and restart the game without a page refresh (just like when the user wins).
+//Make page reload if user loses and add the lose to their loss counter visible using HTML.
